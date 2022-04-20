34a84340559b31ef0494cc7420e3d88f447daaac
bbkincso <bbkincso@gmail.com>
2022-04-20 04:09:52 +0100

    added test

--- src/test/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplicationTests.java
+++ src/test/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ait.microservice.springhelloworld;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+
+
 
 @SpringBootTest
 class SpringHelloWorldApplicationTests {
 
+    HelloService helloService;
+
+    @Before
+    public void setUp() throws Exception {
+        helloService = new HelloService();
+    }
+
+
     @Test
-    void contextLoads() {
+    public void test() {
+        Assert.assertEquals("Hello World 2022...", helloService.index());
     }
 
 }

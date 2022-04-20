005538ff434973bbc58f482e47aae7799097794c
bbkincso <bbkincso@gmail.com>
2022-04-20 02:36:45 +0100

    Tomcat

--- src/main/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplication.java
+++ src/main/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplication.java
@@ -2,9 +2,10 @@ package com.ait.microservice.springhelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringHelloWorldApplication {
+public class SpringHelloWorldApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
